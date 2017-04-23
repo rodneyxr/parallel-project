2ae579c3b263d6ab180dfe28af9fb0eff25da519
jheaps <sey967@my.utsa.edu>
2017-04-23 15:40:44 -0500

    change

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <ctime>
 #include "tools/entitygenerator.hpp"
 #include "tools/stopwatch.h"
+#include "gjk/gjk.hpp"
 
 #define NUMBER_OF_ENTITIES 10
 
@@ -24,6 +25,23 @@ int main() {
     }
     watch.stop();
     std::cout << watch << std::endl;
+
+    int countTrue = 0;
+    int countFalse = 0;
+    for(int j = 0; j < virtual_environment.GetEntities()->size(); j++){
+        Entity e1 = virtual_environment.GetEntities()->at(j);
+        for(int k = j + 1; k < virtual_environment.GetEntities()->size(); k++) {
+            Entity e2 = virtual_environment.GetEntities()->at(k);
+            bool hit = gjk::Run(e2, e1);
+            if (hit)
+                countTrue++;
+            else
+                countFalse++;
+        }
+    }
+
+    std::cout << "Test Passed: " << countTrue << " times." << std::endl;
+    std::cout << "Test Failed: " << countFalse << " times." << std::endl;
 
     return 0;
 }
